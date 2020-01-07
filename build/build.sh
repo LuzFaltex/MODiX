2fcbb5c46e8ce1dc969d358f523e9ac1c0ba9bce
foxtrek64 <foxtrek64@users.noreply.github.com>
2020-01-06 21:17:20 -0600

    Revert changes

--- build/build.sh
+++ build/build.sh
@@ -4,5 +4,4 @@ yum install curl -y
 curl -sL https://rpm.nodesource.com/setup_10.x | bash -
 yum install nodejs -y
 
-rm -rf /app/*
 dotnet publish /opt/MODiX/Modix.sln -c Release -r linux-x64 --self-contained -o /app
