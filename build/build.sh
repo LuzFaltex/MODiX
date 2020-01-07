92b36ad8d166cbe6c05b1cbc0b6d46f92ddcfe1c
foxtrek64 <foxtrek64@users.noreply.github.com>
2020-01-06 21:15:57 -0600

    Just remove items in the app dir instead

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,5 @@ yum install curl -y
 curl -sL https://rpm.nodesource.com/setup_10.x | bash -
 yum install nodejs -y
 
-rm -rf /app
-mkdir /app
+rm -rf /app/*
 dotnet publish /opt/MODiX/Modix.sln -c Release -r linux-x64 --self-contained -o /app
