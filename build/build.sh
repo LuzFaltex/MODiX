168555200d2e43c7c3d84109698f60c21457b7f8
foxtrek64 <foxtrek64@users.noreply.github.com>
2020-01-06 21:13:01 -0600

    Remove and recreate app dir

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,6 @@ yum install curl -y
 curl -sL https://rpm.nodesource.com/setup_10.x | bash -
 yum install nodejs -y
 
+rm -rf /app
+mkdir /app
 dotnet publish /opt/MODiX/Modix.sln -c Release -r linux-x64 --self-contained -o /app
