5c122c40e86856414d5f8635729488e854dab90d
foxtrek64 <foxtrek64@users.noreply.github.com>
2020-01-06 19:25:52 -0600

    Update channel id, clean up method

--- Modix.Bot/Extensions/GuildChannelExtensions.cs
+++ Modix.Bot/Extensions/GuildChannelExtensions.cs
@@ -5,12 +5,13 @@ namespace Modix.Bot.Extensions
 {
     internal static class GuildChannelExtensions
     {
-        private static readonly ulong[] PrivateChannels = { 606205846854303755 };
+        private static readonly ulong[] PrivateChannels = { 606206696502722577 };
         public static bool IsPublic(this IGuildChannel channel)
         {
             if (channel is { })
             {
-                return PrivateChannels.Any(x => x.Equals(channel.Id));
+                // If the current channel is NOT contained in the list of private channels
+                return !PrivateChannels.Contains(channel.Id);
             }
 
             return false;
